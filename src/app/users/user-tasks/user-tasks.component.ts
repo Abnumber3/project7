bf01a7a61d17adf26f39ad63a86c4a5b2f3a453c
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-06-30 23:12:48 -0400

    routerLink and params practice

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+ 
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+
+  username: string =''
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParamMap.subscribe({
+      next: (params) =>{
+        this.username = params.get('name')|| '';
+      }
+    })
+  }
+}
